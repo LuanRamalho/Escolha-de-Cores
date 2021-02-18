5183846eb980b2d88f7328940cb5407854eaa642
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-02-18 13:37:34 -0300

    Add files via upload

---
+++ ShowColors2.java
@@ -0,0 +1,13 @@
+package Escolha_de_Cores;
+import javax.swing.JFrame;
+
+public class ShowColors2 
+{
+    // executa o aplicativo
+    public static void main(String[] args) 
+    {
+        ShowColors2JFrame application = new ShowColors2JFrame();
+        application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
